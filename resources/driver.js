103b3259d6eebcb6110fd500b75a07069b837ea7
weebney <weebney@gmail.com>
2022-08-10 17:27:41 -0400

    Fix: remove pagination from request when unnecessary

--- resources/driver.js
+++ resources/driver.js
@@ -2,25 +2,35 @@ const retrieve = async () => {
     const inputURL = window.location.href
     var slug = inputURL.substr(inputURL.lastIndexOf('?') + 1);
 
+    let response = [];
+    let arenaContent = [];
+    let parray = [];
+    let array = [];
+
     const fetchID = async () => {
         const slugResponse = await fetch('https://api.are.na/v2/channels/' + slug);
         const slugP = await slugResponse.json();
         let id = slugP["id"];
 
-        let length = Math.ceil((slugP["length"]) / 100) * 100;
+        if (slugP["length"] <= 100) {
+            let length = slugP["length"];
+            response = await fetch('https://api.are.na/v2/channels/' + id + '/contents?per=' + length);
 
+        } else {
+            let length = Math.ceil((slugP["length"]) / 100) * 100;
+            for (let i = 1; i < length / 100; i += 1) {
+                response = await fetch('https://api.are.na/v2/channels/' + id + '/contents?per=100&page=' + i);
 
-        let response = [];
-        let arenaContent = [];
-        let parray = [];
-        let array = [];
+            }
 
-        for (let i = 1; i < length / 100; i += 1) {
-            response = await fetch('https://api.are.na/v2/channels/' + id + '/contents?per=100&page=' + i);
-            arenaContent = await response.json();
-            parray = arenaContent.contents;
-            array = array.concat(parray);
         }
+        arenaContent = await response.json();
+        parray = arenaContent.contents;
+        array = array.concat(parray);
+
+
+
+
 
         for (let i = 0; i < array.length; i++) {
             if (array[i].class == "Image") {
