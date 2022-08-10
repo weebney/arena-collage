4b6815d5cb0eade0ecd042bdd58142c341de719f
weebney <weebney@gmail.com>
2022-08-10 00:52:37 -0400

    Update columns.js

--- columns.js
+++ columns.js
@@ -74,8 +74,10 @@ function proceed() {
     var mode = document.querySelector('input[name="mode"]:checked').value;
     var courl = document.getElementById("collection").value;
     var passurl = courl.substr(courl.lastIndexOf('/') + 1);
-    var finalurl = "/" + mode + ".html?" + passurl;
-    location.href = finalurl;
+    var finalurl = mode + ".html?" + passurl;
+    location.href = window.location.href + finalurl;
+
+
 }
 
 var loadState = 3;
